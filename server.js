b6a8989c17b56bdf0f190ad1eb5661d71f29cce8
Danielle Bowman <dcbowman6@yahoo.com>
2019-08-02 18:19:31 -0400

    updated route paths

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.static(__dirname + 'dist/SugarShop'));
 
 app.get('/*', function(req,res){
-    res.sendFile(path.join(__dirname+'dist/SugarShop/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/SugarShop/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
